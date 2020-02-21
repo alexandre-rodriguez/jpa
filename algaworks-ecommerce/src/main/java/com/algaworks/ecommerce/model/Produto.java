80b765f7d2a9aa91381eb61bf366aacb42a1c604
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-21 20:01:01 -0300

    Criando o EntityManager

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -24,6 +24,30 @@ public class Produto {
         this.id = id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
