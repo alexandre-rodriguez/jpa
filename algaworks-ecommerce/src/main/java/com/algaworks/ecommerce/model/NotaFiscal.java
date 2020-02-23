2d57026fd9fc731954449793b38e2bd2adc173de
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-23 13:19:48 -0300

    Mapeando as entidades e customizando os nomes das tabelas e colunas

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -4,8 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -13,16 +15,19 @@ import java.util.Date;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "nota_fiscal")
 public class NotaFiscal {
 
     @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
+    @Column(name = "pedido_id")
     private Integer pedidoId;
 
     private String xml;
 
+    @Column(name = "data_emissao")
     private Date dataEmissao;
 
 }
--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -4,20 +4,24 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigInteger;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "pagamento_cartao")
 public class PagamentoCartao {
 
     @EqualsAndHashCode.Include
     @Id
     private Integer id;
 
+    @Column(name = "pedido_id")
     private Integer pedidoId;
 
     private StatusPagamento status;
