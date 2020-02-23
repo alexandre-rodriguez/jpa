966859243695c532d09f07010ae5794bc3c77799
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-23 17:23:56 -0300

    Removendo objetos referenciados por outras entidades

--- algaworks-ecommerce/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ algaworks-ecommerce/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -4,3 +4,6 @@ insert into produto (id, nome, preco, descricao) values (3, 'Câmera GoPro Hero 
 insert into cliente (id, nome) values (1, 'Fernando Medeiros');
 insert into cliente (id, nome) values (2, 'Marcos Mariano');
 insert into cliente (id, nome) values (101, 'José da Silva');
+
+insert into pedido (id, cliente_id, data_pedido, total, status) values (1, 1, sysdate(), 100.0, 'AGUARDANDO');
+insert into item_pedido (id, pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 1, 5.0, 2);
