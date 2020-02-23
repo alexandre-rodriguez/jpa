69e3ff2b4aa743e67ab776129818cc8c6bef6d5e
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-23 18:39:50 -0300

    Exercício: mapeando relacionamento um-para-um

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosOneToOneTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class RelacionamentosOneToOneTest extends EntityManagerTest {
 
@@ -28,6 +29,25 @@ public class RelacionamentosOneToOneTest extends EntityManagerTest {
         Assert.assertNotNull(pedidoVerificacao.getPagamento());
 
 
+    }
+
+    @Test
+    public void verificarRelacionamentoPedidoNotaFiscal() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setXml("TESTE");
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getNotaFiscal());
     }
 
 }
