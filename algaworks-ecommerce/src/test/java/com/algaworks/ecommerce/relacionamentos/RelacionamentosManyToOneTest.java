bcde0fe443428e7c0dc186d3eeffbb034919dbc1
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-26 16:03:33 -0300

    Exercício: usando @IdClass

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
@@ -35,7 +35,8 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest {
 
     @Test
     public void verificarRelacionamentoItemPedido() {
-        /*
+        entityManager.getTransaction().begin();
+
         Cliente cliente = entityManager.find(Cliente.class, 1);
         Produto produto = entityManager.find(Produto.class, 1);
 
@@ -45,24 +46,24 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest {
         pedido.setTotal(BigDecimal.TEN);
         pedido.setCliente(cliente);
 
+        entityManager.persist(pedido);
+
         ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPedidoId(pedido.getId());
+        itemPedido.setProdutoId(produto.getId());
         itemPedido.setPrecoProduto(produto.getPreco());
         itemPedido.setQuantidade(BigInteger.ONE);
         itemPedido.setPedido(pedido);
         itemPedido.setProduto(produto);
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
         entityManager.persist(itemPedido);
         entityManager.getTransaction().commit();
 
         entityManager.clear();
 
-        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
         Assert.assertNotNull(itemPedidoVerificacao.getPedido());
         Assert.assertNotNull(itemPedidoVerificacao.getProduto());
-        */
-
     }
 
 }
