9b3a26e3d019c1d335b450ec45af5d7605809180
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-26 18:00:46 -0300

    Mapeando coleções de objetos embutidos com @ElementCollection

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -50,4 +50,10 @@ public class Produto {
             joinColumns = @JoinColumn(name = "produto_id"))
     @Column(name = "tag")
     private List<String> tags;
+
+
+    @ElementCollection
+    @CollectionTable(name = "produto_atributo",
+            joinColumns = @JoinColumn(name = "produto_id"))
+    private List<Atributo> atributos;
 }
