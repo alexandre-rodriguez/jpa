740743a56d068893cac11dc538edc033ed60b31b
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-02-29 19:15:54 -0300

    Fazendo o join e usando o fetch

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -14,6 +14,21 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
 
     @Test
+    public void usarJoinFetch() {
+        String jpql = "select p from Pedido p " +
+                "join fetch p.itens " +
+                "left join fetch p.pagamento " +
+                "join fetch p.cliente " +
+                "left join fetch p.notaFiscal " +
+                "where p.id = 1 ";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        Pedido lista = typedQuery.getSingleResult();
+
+        Assert.assertFalse(lista.getItens().isEmpty());
+    }
+
+    @Test
     public void fazerLeftJoin() {
         String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
 
