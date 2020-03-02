dce8ad34d449311f311cb660468ddf065bb7beb6
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-03-02 12:48:13 -0300

    Usando a expressão case

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -16,7 +16,35 @@ import java.util.List;
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
     @Test
-    public void usarExpressaoDifearente() {
+    public void usarExpressaoCase() {
+        /*
+        String jpql = "select p.id, " +
+                "case p.status " +
+                "   when 'PAGO' then 'Está pago' " +
+                "   when 'CANCELADO' then 'Foi cancelado' " +
+                "   else 'Está aguardando' " +
+                "end " +
+                "from Pedido p";
+        */
+
+        String jpql = "select p.id, " +
+                "case type(p.pagamento) " +
+                "   when PagamentoBoleto then 'Pago com boleto' " +
+                "   when PagamentoCartao then 'Pago com cartão' " +
+                "   else 'Está aguardando' " +
+                "end " +
+                "from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    public void usarExpressaoDiferente() {
         String jpql = "select p from Produto p where p.preco <> 100";
 
         TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
