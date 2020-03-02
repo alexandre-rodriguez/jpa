658cc5eb2791970f114d9fef538744a1953f6146
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-03-02 14:38:20 -0300

    Criando subqueries com any

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/SubqueriesTest.java
@@ -14,6 +14,28 @@ import java.util.TimeZone;
 public class SubqueriesTest extends EntityManagerTest {
 
     @Test
+    public void perquisarComAny() {
+        // Podemos usar o ANY e o SOME
+
+        // Todos os produtos que já foram vendidos por um preço diferente do atual
+        String jpql = "select p from Produto p where " +
+                "p.preco <> ANY(select precoProduto from ItemPedido where produto = p)";
+
+        // Todos os produtos que já foram vendidos, pelo menos, uma vez pelo preço atual.
+        /*
+        String jpql = "select p from Produto p where " +
+                "p.preco = ANY (select precoProduto from ItemPedido where produto = p)";
+        */
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+
+    @Test
     public void perquisarComAll() {
 
         // Todos os produtos que não foram vendidos mais depois que encareceram
