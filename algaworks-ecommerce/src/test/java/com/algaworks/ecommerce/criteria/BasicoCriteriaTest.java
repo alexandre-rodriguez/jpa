2bd257912fd0a560c76b69b1e528a557ab8c25fe
Alexandre Rodriguez <alexandre.p.rodriguez@gmail.com>
2020-03-03 13:54:29 -0300

    Usando tuple para uma projeção

--- algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -7,6 +7,7 @@ import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -15,6 +16,24 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void projetarOResultadoTuple() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        // CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createQuery(Tuple.class);
+        CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery();
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        // criteriaQuery.multiselect(root.get("id"), root.get("nome"));
+        criteriaQuery.select(criteriaBuilder.tuple(root.get("id").alias("id"), root.get("nome").alias("nome")));
+
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Tuple> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(t -> System.out.println("ID: " + t.get("id") + ", nome: " + t.get("nome")));
+    }
 
     @Test
     public void projetarOResultado() {
